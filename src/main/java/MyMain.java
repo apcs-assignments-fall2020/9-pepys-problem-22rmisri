94a98acd857520aff0c80bb33f7c1e86cf89fc4f
22rmisri <22rmisri@athenian.org>
2020-10-02 20:46:19 +0000

    Wrote code for pepys problem

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,22 +5,60 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int c = 0;
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 6; j++) {
+                int val = (int) (Math.random()* 6) + 1;
+                if ( val == 6) {
+                    c = c + 1;
+                    break;
+                }
+            }
+        }
+        return (c/100000.0 * 100.0);
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int c = 0;
+        for (int i = 0; i < 100000; i++) {
+            int c2 = 0;
+            for (int j = 0; j < 12; j++) {
+                int val = (int) (Math.random()* 6) + 1;
+                if ((val == 6 ) && (c2 == 1)) {
+                    c++;
+                    break;
+                    }
+                else if (val == 6) {
+                    c2++;
+                }
+                
+            }
+        }
+        return (c/100000.0 * 100.0);
     }
+    
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+       int c = 0;
+        for (int i = 0; i < 100000; i++) {
+            int c2 = 0;
+            for (int j = 0; j < 18; j++) {
+                int val = (int) (Math.random()* 6) + 1;
+                if ((val == 6 ) && (c2 == 2)) {
+                    c++;
+                    break;
+                    }
+                else if (val == 6) {
+                    c2++;
+                }
+                
+            }
+        }
+        return (c/100000.0 * 100.0);
     }
 
 
